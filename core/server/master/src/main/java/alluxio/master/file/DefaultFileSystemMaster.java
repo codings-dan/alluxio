a57acbeb4422fde4428e5509eff03b70ddea57d7
maobaolong <307499405@qq.com>
2022-05-07 09:43:03 +0800

    Optimize the getLostFile to avoid one copy
    ### What changes are proposed in this pull request?
    
    Please outline the changes and how this PR fixes the issue.
    
    ### Why are the changes needed?
    
    Please clarify why the changes are needed. For instance,
    1. If you propose a new API, clarify the use case for a new API.
    2. If you fix a bug, describe the bug.
    
    ### Does this PR introduce any user facing changes?
    
    Please list the user-facing changes introduced by your change, including
    1. change in user-facing APIs
    2. addition or removal of property keys
    3. webui
    
    pr-link: Alluxio/alluxio#15512
    change-id: cid-a6c809da06aef5dc2e2402a3b00ddfe3a69938c7
    (cherry picked from commit 31e6398ffbc2d67127200753af35388c102de1bd)

--- core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
+++ core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -3002,7 +3002,7 @@ public class DefaultFileSystemMaster extends CoreMaster
   }
 
   @Override
-  public List<Long> getLostFiles() {
+  public Set<Long> getLostFiles() {
     Set<Long> lostFiles = new HashSet<>();
     Iterator<Long> iter = mBlockMaster.getLostBlocksIterator();
     while (iter.hasNext()) {
@@ -3012,7 +3012,7 @@ public class DefaultFileSystemMaster extends CoreMaster
       long fileId = IdUtils.createFileId(containerId);
       lostFiles.add(fileId);
     }
-    return new ArrayList<>(lostFiles);
+    return lostFiles;
   }
 
   /**
